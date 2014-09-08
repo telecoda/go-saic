5ee8a4325f32edc142e974724ae3006f6703b2b7
Rob Baines <rob.baines@hailocab.com>
2014-09-08 23:57:51 +0100

    change thumbnail sizes

--- imageutils/thumbnails.go
+++ imageutils/thumbnails.go
@@ -14,7 +14,7 @@ import (
 	"github.com/telecoda/go-saic/models"
 )
 
-const THUMBNAIL_WIDTH = 40
+const THUMBNAIL_WIDTH = 64
 
 // create a thumbnail for each image in db
 func CreateThumbnails(thumbnailImagesDir string) error {
